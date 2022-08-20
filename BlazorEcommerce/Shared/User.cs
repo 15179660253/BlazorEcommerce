151cc7ab1ee2593447c27fb12df3056bb15aa493
刘超 <2743996794@qq.com>
2022-08-20 16:30:49 +0800

    AdminCategories(P166)

--- BlazorEcommerce/Shared/User.cs
+++ BlazorEcommerce/Shared/User.cs
@@ -15,6 +15,6 @@ namespace BlazorEcommerce.Shared
         public byte[] PasswordSalt { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
         public Address Address { get; set; }
-        //public string Role { get; set; } = "Customer";
+        public string Role { get; set; } = "Customer";
     }
 }
